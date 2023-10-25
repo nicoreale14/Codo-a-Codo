86f4685a4dec32bfcab9944dc369bb4bb9e78c4a
nicoreale14 <84485629+nicoreale14@users.noreply.github.com>
2023-10-25 20:05:20 -0300

    ertert

--- js/app.js
+++ js/app.js
@@ -13,55 +13,38 @@ function validarDatos(evento) {
   }
 
 
-	var patente = document.getElementById('patente').value;
-            if (patente.length < 7) {
-                alert('La Patente no es válida. La misma debe contener al menos siete caracteres alfanuméricos.');
-                return;
-            }
-	
-	var fecha = document.getElementById('fecha').value;
-            if (fecha.length == 0) {
-                alert('El campo Fecha / Horario se encuentra sin completar.');
-                return;
-            }
-	
-            this.submit();
-		alert('Su turno se ha reservado correctamente. Muchas gracias.');	
-        				}
-
-
-function traer() {
-fetch	('http://worldtimeapi.org/api/timezone/America/Argentina/Salta')
-.then(res => res.json())
-.then(res => {
-console.log(res)
-contenido.innerHTML= `<p>Fecha (AAAA-MM-DD) / horario / zona horaria actual: ${res.datetime}</p>`
-})
-}
-
-  var telefono = document.getElementById("telefono").value;
-  if (telefono.length < 12) {
-    alert(
-      "El número de teléfono ingresado no es válido. Debe contener al menos diez caracteres numéricos, incluyendo el código de área. Por ejemplo: 11-xxxx-xxxx."
-    );
-    return;
-  }
-
-  var patente = document.getElementById("patente").value;
+  var patente = document.getElementById('patente').value;
   if (patente.length < 7) {
-    alert(
-      "La Patente no es válida. La misma debe contener al menos siete caracteres alfanuméricos."
-    );
+    alert('La Patente no es válida. La misma debe contener al menos siete caracteres alfanuméricos.');
     return;
   }
 
-  var fecha = document.getElementById("fecha").value;
+  var fecha = document.getElementById('fecha').value;
   if (fecha.length == 0) {
-    alert("El campo Fecha / Horario se encuentra sin completar.");
+    alert('El campo Fecha / Horario se encuentra sin completar.');
     return;
   }
 
   this.submit();
-  alert("Su turno se ha reservado correctamente. Muchas gracias.");
+  alert('Su turno se ha reservado correctamente. Muchas gracias.');
+  return;
 }
+
+var telefono = document.getElementById("telefono").value;
+var patente = document.getElementById("patente").value;
+if (patente.length < 7) {
+  alert("La Patente no es válida. La misma debe contener al menos siete caracteres alfanuméricos.");
+  return;
+}
+
+var fecha = document.getElementById("fecha").value;
+if (fecha.length == 0) {
+  alert("El campo Fecha / Horario se encuentra sin completar.");
+  return;
+}
+
+this.submit();
+alert("Su turno se ha reservado correctamente. Muchas gracias.");
+
+
 
---
+++ js/newFile.js
@@ -0,0 +1,6 @@
+if (telefono.length < 12) {
+    alert(
+        "El número de teléfono ingresado no es válido. Debe contener al menos diez caracteres numéricos, incluyendo el código de área. Por ejemplo: 11-xxxx-xxxx."
+    );
+    return;
+}
