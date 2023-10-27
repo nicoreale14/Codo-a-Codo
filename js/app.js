3010c55d540a9a2baecca06b1af9be56268a8a0e
nicoreale14 <84485629+nicoreale14@users.noreply.github.com>
2023-10-27 17:34:20 -0300

    ultimos arreglos

--- js/app.js
+++ js/app.js
@@ -12,6 +12,11 @@ function validarDatos(evento) {
     return;
   }
 
+  var telefono = document.getElementById("telefono").value;
+  if (telefono.length < 7) {
+    alert('La Patente no es válida. La misma debe contener al menos siete caracteres alfanuméricos.');
+    return;
+  }
 
   var patente = document.getElementById('patente').value;
   if (patente.length < 7) {
@@ -30,21 +35,16 @@ function validarDatos(evento) {
   return;
 }
 
-var telefono = document.getElementById("telefono").value;
-var patente = document.getElementById("patente").value;
-if (patente.length < 7) {
-  alert("La Patente no es válida. La misma debe contener al menos siete caracteres alfanuméricos.");
-  return;
-}
-
-var fecha = document.getElementById("fecha").value;
-if (fecha.length == 0) {
-  alert("El campo Fecha / Horario se encuentra sin completar.");
-  return;
-}
-
 this.submit();
 alert("Su turno se ha reservado correctamente. Muchas gracias.");
 
+function traer() {
+  fetch('http://worldtimeapi.org/api/timezone/America/Argentina/Salta')
+    .then(res => res.json())
+    .then(res => {
 
+      console.log(res)
+      contenido.innerHTML = `<p>Fecha (AAAA-MM-DD) / horario / zona horaria actual: ${res.datetime}</p>`
+    })
+}
 
