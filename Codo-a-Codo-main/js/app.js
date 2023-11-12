e9748f33419fc2a954479bd4e2270bf607c02550
CharlieMarSar <147785697+CharlieMarSar@users.noreply.github.com>
2023-11-11 22:48:46 -0300

    Add files via upload

---
+++ Codo-a-Codo-main/js/app.js
@@ -0,0 +1,50 @@
+document.addEventListener("DOMContentLoaded", function () {
+  document
+    .getElementById("formulario")
+    .addEventListener("submit", validarDatos);
+});
+
+function validarDatos(evento) {
+  evento.preventDefault();
+  var nombre = document.getElementById("nombre").value;
+  if (nombre.length == 0) {
+    alert("El campo Nombre se encuentra sin completar.");
+    return;
+  }
+
+  var telefono = document.getElementById("telefono").value;
+  if (telefono.length < 7) {
+    alert('La Patente no es válida. La misma debe contener al menos siete caracteres alfanuméricos.');
+    return;
+  }
+
+  var patente = document.getElementById('patente').value;
+  if (patente.length < 7) {
+    alert('La Patente no es válida. La misma debe contener al menos siete caracteres alfanuméricos.');
+    return;
+  }
+
+  var fecha = document.getElementById('fecha').value;
+  if (fecha.length == 0) {
+    alert('El campo Fecha / Horario se encuentra sin completar.');
+    return;
+  }
+
+  this.submit();
+  alert('Su turno se ha reservado correctamente. Muchas gracias.');
+  return;
+}
+
+this.submit();
+alert("Su turno se ha reservado correctamente. Muchas gracias.");
+
+function traer() {
+  fetch('http://worldtimeapi.org/api/timezone/America/Argentina/Salta')
+    .then(res => res.json())
+    .then(res => {
+
+      console.log(res)
+      contenido.innerHTML = `<p>Fecha (AAAA-MM-DD) / horario / zona horaria actual: ${res.datetime}</p>`
+    })
+}
+
