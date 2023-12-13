eea8b15bedf659529df81e259caaa2e8b0dfdb5d
nicoreale14 <nicolassebastianreale@gmail.com>
2023-12-13 11:44:36 -0300

    incorporacion del crud

--- js/app.js
+++ js/app.js
@@ -50,3 +50,15 @@ function traer() {
       contenido.innerHTML = `<p>Fecha (AAAA-MM-DD) / horario / zona horaria actual: ${res.datetime}</p>`;
     });
 }
+
+document.getElementById(
+  "header"
+).innerHTML = ` <nav class="navbar navbar-expand-sm
+navbar-light bg-light">
+<div class="container">
+<a class="navbar-brand" href="../templates/productos.html">CRUD</a>
+
+
+
+</div>
+</nav>`;
