915fccc42df349df3126f8acb143e333ff7caa6b
nicoreale14 <84485629+nicoreale14@users.noreply.github.com>
2023-12-21 16:19:13 -0300

    Update productos.js

--- js/productos.js
+++ js/productos.js
@@ -3,9 +3,9 @@ createApp({
   data() {
     return {
       productos: [],
-      url: "http://localhost:5000/productos",
+  
       // si el backend esta corriendo local usar localhost 5000(si no lo subieron a pythonanywhere)
-      url: "https://comision23541.pythonanywhere.com/productos", // si ya lo subieron a pythonanywhere
+      url: "https://nicoreale.pythonanywhere.com/productos", // si ya lo subieron a pythonanywhere
       error: false,
       cargando: true,
       /*atributos para el guardar los valores del formulario */
